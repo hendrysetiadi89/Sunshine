81a64262bb62acac53ca526ab1c8f29bc14c2f12
hendrysetiadi89 <hendrysetiadi@bahaso.com>
2016-11-15 15:31:28 +0700

    Day3

---
+++ app/src/main/java/com/sunshineapp/activity/DetailActivity.java
@@ -0,0 +1,27 @@
+package com.sunshineapp.activity;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import com.sunshineapp.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_detail);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Detail Cuaca");
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+
+    }
+}
