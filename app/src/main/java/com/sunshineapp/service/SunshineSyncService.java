956dae0c6875db1c1326a19fb2858984fe0b769d
hendrysetiadi89 <hendrysetiadi@bahaso.com>
2016-11-27 11:32:00 +0700

    accountauth

---
+++ app/src/main/java/com/sunshineapp/service/SunshineSyncService.java
@@ -0,0 +1,33 @@
+package com.sunshineapp.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+/**
+ * Created by hendrysetiadi on 27/11/2016.
+ */
+
+public class SunshineSyncService extends Service {
+    private static final Object sSyncAdapterLock = new Object();
+    private static SunshineSyncAdapter mSyncAdapter = null;
+
+    public SunshineSyncService() {
+        super();
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        synchronized (sSyncAdapterLock) {
+            if (mSyncAdapter == null) {
+                mSyncAdapter = new SunshineSyncAdapter(getApplicationContext(), true);
+            }
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent arg0) {
+        return mSyncAdapter.getSyncAdapterBinder();
+    }
+}
