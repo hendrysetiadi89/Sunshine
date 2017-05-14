2fc73cc0896e0bca8af74ddf64031af603abd461
hendrysetiadi89 <hendrysetiadi@bahaso.com>
2017-05-14 09:11:16 +0700

    add Stetho for inspect database

---
+++ app/src/main/java/com/sunshineapp/application/MyApplication.java
@@ -0,0 +1,36 @@
+package com.sunshineapp.application;
+
+import android.app.Application;
+
+import com.facebook.stetho.Stetho;
+
+/**
+ * Created by Test on 5/14/2017.
+ */
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Create an InitializerBuilder
+        Stetho.InitializerBuilder initializerBuilder =
+                Stetho.newInitializerBuilder(this);
+
+        // Enable Chrome DevTools
+        initializerBuilder.enableWebKitInspector(
+                Stetho.defaultInspectorModulesProvider(this)
+        );
+
+        // Enable command line interface
+        initializerBuilder.enableDumpapp(
+                Stetho.defaultDumperPluginsProvider(this.getApplicationContext())
+        );
+
+        // Use the InitializerBuilder to generate an Initializer
+        Stetho.Initializer initializer = initializerBuilder.build();
+
+        // Initialize Stetho with the Initializer
+        Stetho.initialize(initializer);
+    }
+}
